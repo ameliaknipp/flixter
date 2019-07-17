e58b471ef6c344fa7ad5bb371f8d3d9a9f37d9f1
ameliaknipp <ameliaknipp@aol.com>
2019-07-17 00:32:53 +0000

    Fixed Image Uploading and Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_lesson
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+      if current_lesson.user != current_user
+          redirect_to_course_sections, alert: 'Error Message Here'
+      end
+      end
+    end
 
   helper_method :current_lesson
   def current_lesson
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_011025) do
+ActiveRecord::Schema.define(version: 2019_07_15_225043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2019_07_09_011025) do
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_enrollments_on_course_id"
     t.index ["user_id", "course_id"], name: "index_enrollments_on_user_id_and_course_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
